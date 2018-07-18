8044c3fbda62e66e751e6bf0fced503b414acaa5
Johan Pretorius <johanp@fractalsolutions.io>
2018-07-18 12:42:22 +0200

    Isin Issuance update

--- contracts/IsinIssuance.sol
+++ contracts/IsinIssuance.sol
@@ -10,6 +10,17 @@ contract PostTrade {
 
 contract IsinIssuance {
 
+    // ==========================================================================
+    // Modifiers
+    // ==========================================================================
+    modifier onlyOwner{
+        require(msg.sender == owner);
+        _;
+    }
+
+    // ==========================================================================
+    // Constructor
+    // ==========================================================================
     constructor (address _postTradeContract) public {
         postTradeContractAddress = _postTradeContract;
         postTradeContract = PostTrade(_postTradeContract);
@@ -18,6 +29,39 @@ contract IsinIssuance {
 
     PostTrade postTradeContract;
     address public postTradeContractAddress;
+    address owner;
+
+    // ==========================================================================
+    // Structs
+    // ==========================================================================
+    struct Security {
+        string ISIN;
+        uint totalIssuedShareCap;
+        string longName;
+        string ticker;
+        bool active;
+        address issuer;
+        address verifier;
+    }
+
+    // ==========================================================================
+    // Variables
+    // ==========================================================================
+    mapping (address => Security[]) public securitiesToBeVerifiedByParty;
+
+    function captureSecurity (string _ISIN, uint _totalIssuedShareCap, string _longName, string _ticker, address _counterParty) public onlyOwner {
+        securitiesToBeVerifiedByParty[_counterParty].push(
+            Security({
+                ISIN: _ISIN,
+                totalIssuedShareCap: _totalIssuedShareCap,
+                longName: _longName,
+                ticker: _ticker,
+                active: false,
+                issuer: msg.sender,
+                verifier: _counterParty
+            })
+        );
+    }
 
     //IsinIssuance.deployed().then(function(instance){return instance.postTradeContractAddress()});
 
