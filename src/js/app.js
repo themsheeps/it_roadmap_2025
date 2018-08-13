1fefc69f70b48ae12efd89dbf33b76b9e953b8c5
themsheeps <johanp@strate.co.za>
2018-08-13 12:29:31 +0200

    start creating front end

--- src/js/app.js
+++ src/js/app.js
@@ -3,40 +3,35 @@ App = {
   contracts: {},
 
   init: function() {
-    // Load pets.
-    $.getJSON('../pets.json', function(data) {
-      var petsRow = $('#petsRow');
-      var petTemplate = $('#petTemplate');
-
-      for (i = 0; i < data.length; i ++) {
-        petTemplate.find('.panel-title').text(data[i].name);
-        petTemplate.find('img').attr('src', data[i].picture);
-        petTemplate.find('.pet-breed').text(data[i].breed);
-        petTemplate.find('.pet-age').text(data[i].age);
-        petTemplate.find('.pet-location').text(data[i].location);
-        petTemplate.find('.btn-adopt').attr('data-id', data[i].id);
-
-        petsRow.append(petTemplate.html());
-      }
-    });
-
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    /*
-     * Replace me...
-     */
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider;
+    } else {
+      // If no injected web3 instance is detected, fall back to Ganache
+      App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
+    }
+    web3 = new Web3(App.web3Provider);
 
     return App.initContract();
   },
 
   initContract: function() {
-    /*
-     * Replace me...
-     */
+    $.getJSON('PostTrade.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
+      var PostTradeArtifact = data;
+      App.contracts.PostTrade = TruffleContract(PostTradeArtifact);
+    
+      // Set the provider for our contract
+      App.contracts.PostTrade.setProvider(App.web3Provider);
+    
+      // Use our contract to retrieve and mark the adopted pets
+      // return App.markAdopted();
+    });
 
-    return App.bindEvents();
+    // return App.bindEvents();
   },
 
   bindEvents: function() {
