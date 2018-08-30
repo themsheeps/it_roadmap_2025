fc8ffac6491489ff6fa94401d6f091586a92dc0f
themsheeps <johanp@strate.co.za>
2018-08-30 10:35:46 +0200

    T+0 settlement working

--- contracts/PostTrade.sol
+++ contracts/PostTrade.sol
@@ -189,7 +189,7 @@ contract PostTrade {
 
     mapping(bytes32 => uint[]) public matchedTradesIdListForISIN;
     mapping(bytes32 => mapping(uint => Trade)) public matchedTradesForISINandId;
-    mapping(bytes32 => mapping(uint => Trade[])) public confirmedTradesForISINandSettlementDate;
+    //mapping(bytes32 => mapping(uint => Trade[])) public confirmedTradesForISINandSettlementDate;
 
     string[] public securitiesList;
 
@@ -248,7 +248,7 @@ contract PostTrade {
 
         securitiesList.push("eZAR");
 
-        balances[keccak256(abi.encodePacked("eZAR"))][owner] += _totalIssuedShareCap;
+        balances[keccak256(abi.encodePacked("eZAR"))][msg.sender] += _totalIssuedShareCap;
     }
 
     function topUp (string _ISIN, uint _amount) public onlyOwner {
@@ -257,6 +257,7 @@ contract PostTrade {
         balances[keccak256(abi.encodePacked(_ISIN))][owner] += _amount;
     }
 
+    // to save gas cost, rather send securities to 0x0 to burn them.
     // function reduceSecurities (string _ISIN, uint _amount) public onlyOwner {
     //     require (securities[keccak256(abi.encodePacked(_ISIN))].active == true, "SMA");
     //     require (securities[keccak256(abi.encodePacked(_ISIN))].totalIssuedShareCap >= _amount, "TB");
@@ -304,11 +305,9 @@ contract PostTrade {
         uint _buyLegId,
         uint _saleLegId,
         uint _tradeId,
-        uint _settlementDate, // Format YYYYMMDD
+        uint _settlementDate, // Format YYYYMMDD or 0 for instant (T+0) settlement
         address _buyerAddress,
         address _sellerAddress,
-        // address _buyerTradeReportingPartyAddress,
-        // address _sellerTradeReportingPartyAddress,
         address _buyerCustodianId,
         address _sellerCustodianId,
         uint _amount,
@@ -448,13 +447,13 @@ contract PostTrade {
             if (matchedTradesForISINandId[_hash][_tradeId].saleConfirmationDateTime > 0) {
 
                 if (matchedTradesForISINandId[_hash][_tradeId].settlementDeadlineDate == 0) {
-                    balances[keccak256(abi.encodePacked("eZAR"))][buyLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].buyLegId].investorAddress] -= buyLegForISINAndId[_hash][_legId].buyPrice;
-                    balances[keccak256(abi.encodePacked("eZAR"))][saleLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].sellLegId].investorAddress] += saleLegForISINAndId[_hash][_legId].salePrice;
-                    balances[keccak256(abi.encodePacked(_ISIN))][saleLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].sellLegId].investorAddress] -= saleLegForISINAndId[_hash][_legId].amount;
-                    balances[keccak256(abi.encodePacked(_ISIN))][buyLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].buyLegId].investorAddress] += buyLegForISINAndId[_hash][_legId].amount;
+                    balances[keccak256(abi.encodePacked("eZAR"))][buyLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].buyLegId].investorAddress] -= buyLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].buyLegId].buyPrice;
+                    balances[keccak256(abi.encodePacked("eZAR"))][saleLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].sellLegId].investorAddress] += saleLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].sellLegId].salePrice;
+                    balances[keccak256(abi.encodePacked(_ISIN))][saleLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].sellLegId].investorAddress] -= saleLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].sellLegId].amount;
+                    balances[keccak256(abi.encodePacked(_ISIN))][buyLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].buyLegId].investorAddress] += buyLegForISINAndId[_hash][matchedTradesForISINandId[_hash][_tradeId].buyLegId].amount;
                 } else {
                     // Once a perfect match has been made, add the fully confirmed trade to a list of trades to be settled on settlement date.
-                    confirmedTradesForISINandSettlementDate[_hash][matchedTradesForISINandId[_hash][_tradeId].settlementDeadlineDate].push(matchedTradesForISINandId[_hash][_tradeId]);
+                    // confirmedTradesForISINandSettlementDate[_hash][matchedTradesForISINandId[_hash][_tradeId].settlementDeadlineDate].push(matchedTradesForISINandId[_hash][_tradeId]);
                     //TODO: Add securities netting
                     //  Do reservation for securities 
                     //    Decuct securities from seller
@@ -511,15 +510,43 @@ contract PostTrade {
     // Helper Console Scripts:
     // ==========================================================================
 /* 
-
+// Must issue Securities from the IsinIssuance Contract - this will not work
 PostTrade.deployed().then(function(instance){return instance.issueSecurity("ZAE001",1000,"Anglo American PLC","ANG")});
+// Check Admin balance
 PostTrade.deployed().then(function(instance){return instance.getBalanceOfSecAndAccount("ZAE001","0x51e63a2E221C782Bfc95f42Cd469D3780a479C15")});
-PostTrade.deployed().then(function(instance){return instance.sendSecurity("ZAE001",600,"0x2AaB2c02Fc5415D23e91CE8Dc230D3A31793CFF8")});
+// Send ANG to seller
+PostTrade.deployed().then(function(instance){return instance.sendSecurity("ZAE001",600,"0x8eA823e5951243bFA7f1Daad4703396260071fB9")});
+// Check seller balance
+PostTrade.deployed().then(function(instance){return instance.getBalanceOfSecAndAccount("ZAE001","0x8eA823e5951243bFA7f1Daad4703396260071fB9")});
+// Optional send security from one account to another
 PostTrade.deployed().then(function(instance){return instance.sendSecurity("ZAE001",500,"0x8ea823e5951243bfa7f1daad4703396260071fb9", {from: "0x2AaB2c02Fc5415D23e91CE8Dc230D3A31793CFF8"})});
+// Check Security details (cash or ISINs)
 PostTrade.deployed().then(function(instance){return instance.getSecurityDetails("ZAE001")});
+// Issue Cash from and into SAMOS account
+PostTrade.deployed().then(function(instance){return instance.issueCash(10000000, {from:"0x3E7Eaa5Bc0ee36b4308B668050535d411a81585D"})});
+// Check new SAMOS cash balance
+PostTrade.deployed().then(function(instance){return instance.getBalanceOfSecAndAccount("eZAR","0x3E7Eaa5Bc0ee36b4308B668050535d411a81585D")});
+// Check which assets have been issued: 0 => ANG, 1 => eZAR
 PostTrade.deployed().then(function(instance){return instance.getSecuritiesListById(0)});
-PostTrade.deployed().then(function(instance){return instance.addPreMatchedTrade("ZAE001",1234,4321,11223344,20180720,"0xED646f6B0cf23C2bfC0dC4117dA42Eb5CCf15ee4","0xA1Ff8eE897ED92E62aE9F30061Ba5f012e804721","0x1c6B96De685481c2d9915b606D4AB1277949b4Bc","0x2d14d5Ae5E54a22043B1eccD420494DAA9513e06",100,4444,{from:"0x0ef2F9c8845da4c9c34BEf02C3213e0Da1306Da0"})});
+// send cash to Buyer
+PostTrade.deployed().then(function(instance){return instance.sendSecurity("eZAR",10000,"0xFb91a2395d9E49b89fcA3dca0959b6eB4Ea08a0B", {from: "0x3E7Eaa5Bc0ee36b4308B668050535d411a81585D"})});
 
+// Report Pre-matched trade for T+X settlement (_ISIN, _buyLegId, _saleLegId, _tradeId, _settlementDate, _buyerAddress, _sellerAddress, _buyerCustodianId, _sellerCustodianId, _amount, _salePrice)
+PostTrade.deployed().then(function(instance){return instance.addPreMatchedTrade("ZAE001",1234,4321,11223344,20180720,"0xFb91a2395d9E49b89fcA3dca0959b6eB4Ea08a0B","0x8eA823e5951243bFA7f1Daad4703396260071fB9","0x1c6B96De685481c2d9915b606D4AB1277949b4Bc","0x2d14d5Ae5E54a22043B1eccD420494DAA9513e06",100,5000,{from:"0x0ef2F9c8845da4c9c34BEf02C3213e0Da1306Da0"})});
+// Report Pre-matched trade for T+0 settlement (_ISIN, _buyLegId, _saleLegId, _tradeId, _settlementDate, _buyerAddress, _sellerAddress, _buyerCustodianId, _sellerCustodianId, _amount, _salePrice)
+PostTrade.deployed().then(function(instance){return instance.addPreMatchedTrade("ZAE001",12345,54321,1122334455,0,"0xFb91a2395d9E49b89fcA3dca0959b6eB4Ea08a0B","0x8eA823e5951243bFA7f1Daad4703396260071fB9","0x1c6B96De685481c2d9915b606D4AB1277949b4Bc","0x2d14d5Ae5E54a22043B1eccD420494DAA9513e06",100,5000,{from:"0x0ef2F9c8845da4c9c34BEf02C3213e0Da1306Da0"})});
+
+// Confirm Buy leg:
+PostTrade.deployed().then(function(instance){return instance.confirmTradeLeg(0, 12345, "ZAE001")});
+// Confirm Sell leg:
+PostTrade.deployed().then(function(instance){return instance.confirmTradeLeg(1, 54321, "ZAE001")});
+
+// Check Buyer's new ANG balance (100)
+PostTrade.deployed().then(function(instance){return instance.getBalanceOfSecAndAccount("ZAE001","0xFb91a2395d9E49b89fcA3dca0959b6eB4Ea08a0B")});
+// Check Seller's new ANG balance (500)
+PostTrade.deployed().then(function(instance){return instance.getBalanceOfSecAndAccount("ZAE001","0x8eA823e5951243bFA7f1Daad4703396260071fB9")});
+// Check Buyer's new cash balance (5000)
+PostTrade.deployed().then(function(instance){return instance.getBalanceOfSecAndAccount("eZAR","0xFb91a2395d9E49b89fcA3dca0959b6eB4Ea08a0B")});
 */ 
     // ==========================================================================
     // TRUFFLE MNEMONIC: latin bonus invest museum gate buffalo fever demand neglect entire session rail
